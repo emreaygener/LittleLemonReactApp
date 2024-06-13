546a1e46621298b9ec16e1fbe6fb5854b5276253
Emre Aygener <emre.aygener@gmail.com>
2024-06-14 02:33:24 +0300

    Semantics and Meta Tags Added

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import footerLogo from "../assets/img/footerlogo.png";
+
+export default function Footer() {
+  return (
+    <footer>
+      <img src={footerLogo} alt="footer logo" />
+      <p>&copy; 2024</p>
+    </footer>
+  );
+}
